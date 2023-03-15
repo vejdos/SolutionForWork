34b2553c19b92172569ef0698a038af7a1037666
vejdos <115791092+vejdos@users.noreply.github.com>
2023-03-15 10:01:30 +0300

    Добавьте файлы проекта.

---
+++ InceptionTask/Figure.cs
@@ -0,0 +1,12 @@
+﻿namespace Exercise
+{
+    public abstract class Figure
+    {
+        private string name;
+        public Figure(string Name)
+        {
+            name = Name;
+        }
+        public abstract double Area();
+    }
+}
